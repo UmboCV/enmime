c23f1d8fcd005280e7500a1b1c9eb881a907860d
Daniel <shechiway@gmail.com>
2020-03-30 23:10:45 +0800

    1. fix head unit test error, should separate semicolon first, and save the original string for existing postfix content-type, ex: Content-Type: text/html; name=file.html; charset="utf-8" 2. increase header length when doing encode content-type is video/mp4; name=XXXXXXXXX, to prevent adding extra char into file name, it's from some customer did follow rfc spec, so we need to do this workaround

--- encode.go
+++ encode.go
@@ -10,6 +10,7 @@ import (
 	"sort"
 	"strings"
 	"time"
+	"unicode"
 
 	"github.com/jhillyerd/enmime/v2/internal/coding"
 	"github.com/jhillyerd/enmime/v2/internal/stringutil"
@@ -203,7 +204,11 @@ func (p *Part) encodeHeader(b *bufio.Writer) error {
 				}
 			}
 			// _ used to prevent early wrapping
-			wb := stringutil.Wrap(76, k, ":_", encv, "\r\n")
+			var strLength = 76
+			if strings.HasPrefix(strings.TrimLeftFunc(v, unicode.IsSpace), "video/mp4; name=") {
+				strLength = 76 * 2
+			}
+			wb := stringutil.Wrap(strLength, k, ":_", encv, "\r\n")
 			wb[len(k)+1] = ' '
 			if _, err := b.Write(wb); err != nil {
 				return err
